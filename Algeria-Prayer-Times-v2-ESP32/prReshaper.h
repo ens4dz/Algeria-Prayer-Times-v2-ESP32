173d30d542ffcc82737e46a7e7931fbe99cb10d3
ens4dz <ens4dz@gmail.com>
2020-07-26 01:10:04 +0100

    Add files via upload

---
+++ Algeria-Prayer-Times-v2-ESP32/prReshaper.h
@@ -0,0 +1,19 @@
+#ifndef prReshaper_h
+#define prReshaper_h
+#include <Arduino.h>
+
+
+// #if defined(ESP8266)
+    // #define HARDWARE "ESP8266"
+    // #include "ESP8266WiFi.h"
+    // #include "ESPAsyncTCP.h"
+    // #include "ESPAsyncWebServer.h"
+// #elif defined(ESP32)
+    // #define HARDWARE "ESP32"
+    // #include "WiFi.h"
+    // #include "AsyncTCP.h"
+    // #include "ESPAsyncWebServer.h"
+// #endif
+
+String prReshaper123(char *Text);
+#endif
